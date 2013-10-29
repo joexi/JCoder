142a8d175f71da0c1fac7f1e31dcc7094090f368
JoeXi <407154812@qq.com>
2013-10-29 15:16:16 +0800

    Initial Commit

---
+++ JCoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JCoder
+//
+//  Created by Joe on 13-10-29.
+//  Copyright (c) 2013年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
